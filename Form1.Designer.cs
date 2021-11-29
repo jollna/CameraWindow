60c19b149286d41a067f8092c97465b1ece8bd7a
jollan <jollna@outlook.com>
2021-11-29 17:43:55 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CameraWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.设置摄像头ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.停止摄像头ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.窗口置顶ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.ContextMenuStrip = this.contextMenuStrip1;
+            this.videoSourcePlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 0);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(302, 266);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.videoSourcePlayer1_MouseDown);
+            this.videoSourcePlayer1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.videoSourcePlayer1_MouseMove);
+            this.videoSourcePlayer1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.videoSourcePlayer1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置摄像头ToolStripMenuItem,
+            this.停止摄像头ToolStripMenuItem,
+            this.窗口置顶ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 92);
+            // 
+            // 设置摄像头ToolStripMenuItem
+            // 
+            this.设置摄像头ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.camera;
+            this.设置摄像头ToolStripMenuItem.Name = "设置摄像头ToolStripMenuItem";
+            this.设置摄像头ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.设置摄像头ToolStripMenuItem.Text = "设置摄像头";
+            // 
+            // 停止摄像头ToolStripMenuItem
+            // 
+            this.停止摄像头ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.stop;
+            this.停止摄像头ToolStripMenuItem.Name = "停止摄像头ToolStripMenuItem";
+            this.停止摄像头ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.停止摄像头ToolStripMenuItem.Text = "停止摄像头";
+            this.停止摄像头ToolStripMenuItem.Click += new System.EventHandler(this.停止摄像头ToolStripMenuItem_Click);
+            // 
+            // 窗口置顶ToolStripMenuItem
+            // 
+            this.窗口置顶ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.cancel;
+            this.窗口置顶ToolStripMenuItem.Name = "窗口置顶ToolStripMenuItem";
+            this.窗口置顶ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.窗口置顶ToolStripMenuItem.Text = "窗口置顶";
+            this.窗口置顶ToolStripMenuItem.Click += new System.EventHandler(this.窗口置顶ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.export;
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 266);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置摄像头ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 停止摄像头ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 窗口置顶ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CameraWindow
+{
+    public partial class Form1 : Form
+    {
+        private MouseDirection direction = MouseDirection.None;//表示拖动的方向，起始为None，表示不拖动
+        private MouseFunction mouseFunction = MouseFunction.None;//表示按下状态，起始为None，表示不执行任何操作
+        private Point mousePoint { get; set; }
+        private FilterInfoCollection videoDevices { get; set; }//所有摄像设备
+        private VideoCaptureDevice videoDevice { get; set; }//摄像设备
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 窗口置顶ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.TopMost == false)
+            {
+                this.TopMost = true;
+                this.窗口置顶ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.checkmark;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.窗口置顶ToolStripMenuItem.Image = global::CameraWindow.Properties.Resources.cancel;
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void videoSourcePlayer1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //当鼠标按下坐标距离窗体右边缘10像素以上且纵坐标距离下边缘也在10像素以上时，设置按下状态为移动模式
+                if (e.Location.X < this.videoSourcePlayer1.Width - 10 && e.Location.Y < this.videoSourcePlayer1.Height - 10)
+                {
+                    mouseFunction = MouseFunction.Move;
+                    mousePoint = new Point(-e.X, -e.Y);
+                }
+                //当鼠标移动时横坐标距离窗体右边缘10像素以内、纵坐标距离窗体下边缘10像素以内时，要将光标变为倾斜的箭头形状，设置按下状态为调整窗体大小模式，同时拖拽方向direction置为MouseDirection.Declining
+                else if (e.Location.X >= this.videoSourcePlayer1.Width - 10 && e.Location.Y > this.videoSourcePlayer1.Height - 10)
+                {
+                    direction = MouseDirection.Declining;
+                    mouseFunction = MouseFunction.Size;
+                }
+                //当鼠标移动时横坐标距离窗体右边缘10像素以内时，要将光标变为倾斜的箭头形状，设置按下状态为调整窗体大小模式，同时拖拽方向direction置为MouseDirection.Herizontal
+                else if (e.Location.X >= this.videoSourcePlayer1.Width - 10)
+                {
+                    direction = MouseDirection.Herizontal;
+                    mouseFunction = MouseFunction.Size;
+                }
+                //同理当鼠标移动时纵坐标距离窗体下边缘10像素以内时，要将光标变为倾斜的箭头形状，同时拖拽方向direction置为MouseDirection.Vertical
+                else if (e.Location.Y >= this.videoSourcePlayer1.Height - 10)
+                {
+                    direction = MouseDirection.Vertical;
+                    mouseFunction = MouseFunction.Size;
+                }
+                else
+                {
+                    this.Cursor = Cursors.Arrow;
+                }
+            }
+        }
+
+        private void videoSourcePlayer1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseFunction == MouseFunction.Move)
+            {
+                Point mouseSet = Control.MousePosition;//获得移动后鼠标的坐标
+                mouseSet.Offset(mousePoint.X, mousePoint.Y);//设置移动后的位置
+                this.Location = mouseSet;
+            }
+            //当鼠标移动时横坐标距离窗体右边缘10像素以内且纵坐标距离下边缘也在10像素以内时，要将光标变为倾斜的箭头形状
+            if (e.Location.X >= this.videoSourcePlayer1.Width - 10 && e.Location.Y > this.videoSourcePlayer1.Height - 10)
+            {
+                this.Cursor = Cursors.SizeNWSE;
+            }
+            //当鼠标移动时横坐标距离窗体右边缘10像素以内时，要将光标变为倾斜的箭头形状
+            else if (e.Location.X >= this.videoSourcePlayer1.Width - 10)
+            {
+                this.Cursor = Cursors.SizeWE;
+            }
+            //同理当鼠标移动时纵坐标距离窗体下边缘10像素以内时，要将光标变为倾斜的箭头形状
+            else if (e.Location.Y >= this.videoSourcePlayer1.Height - 10)
+            {
+                this.Cursor = Cursors.SizeNS;
+            }
+            //设置光标为鼠标状态
+            else
+            {
+                this.Cursor = Cursors.Arrow;
+            }
+
+            //获取鼠标
+            if (direction != MouseDirection.None)
+            {
+                ResizeWindow();
+            }
+        }
+
+        private void videoSourcePlayer1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseFunction = MouseFunction.None;
+            direction = MouseDirection.None;
+        }
+
+        /// <summary>
+        /// 调整窗体大小
+        /// </summary>
+        private void ResizeWindow()
+        {
+            //判断鼠标按下状态，只有为调整窗体大小状态才能调整窗口大小
+            if (mouseFunction != MouseFunction.Size) return;
+            //MousePosition的参考点是屏幕的左上角，表示鼠标当前相对于屏幕左上角的坐标this.left和this.top的参考点也是屏幕，属性MousePosition是该程序的重点
+            if (direction == MouseDirection.Declining)
+            {
+                //此行代码在mousemove事件中已经写过，在此再写一遍，并不多余，一定要写
+                this.Cursor = Cursors.SizeNWSE;
+                //下面是改变窗体宽和高的代码，不明白的可以仔细思考一下
+                this.Width = MousePosition.X - this.Left;
+                this.Height = MousePosition.Y - this.Top;
+            }
+            if (direction == MouseDirection.Herizontal)
+            {
+                this.Cursor = Cursors.SizeWE;
+                this.Width = MousePosition.X - this.Left;
+            }
+            else if (direction == MouseDirection.Vertical)
+            {
+                this.Cursor = Cursors.SizeNS;
+                this.Height = MousePosition.Y - this.Top;
+            }
+            //即使鼠标按下，但是不在窗口右和下边缘，那么也不能改变窗口大小
+            else
+            {
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+        /// <summary>
+        /// 枚举鼠标位于窗体的不同状态
+        /// </summary>
+        public enum MouseDirection
+        {
+            Herizontal,//水平方向拖动，只改变窗体的宽度
+            Vertical,//垂直方向拖动，只改变窗体的高度
+            Declining,//倾斜方向，同时改变窗体的宽度和高度
+            None//不做标志，即不拖动窗体改变大小
+        }
+        /// <summary>
+        /// 枚举鼠标按下位置的状态
+        /// </summary>
+        public enum MouseFunction
+        {
+            Move,//移动窗体功能
+            Size,//调整窗体大小功能
+            None //不带任何功能
+        }
+
+        private void 停止摄像头ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//得到所有接入的摄像设备
+        }
+    }
+}
